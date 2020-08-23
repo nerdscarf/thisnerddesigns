437b69c67808cf480671cf64f4a665e1d85fe723
nerdscarf <tiph@nerdscarf.com>
2020-08-23 16:30:19 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Butts. All of the Butts
         </p>
         <a
           className="App-link"
